a363fd61daf653b521f8ffb992963f9fb31023cc
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-14 15:18:37 +0100

    add ticket layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,15 +24,33 @@ function generateTicket(event) {
     // Biglietto minorenni
     if (ageRange == 'Minorenne') {
         let ticketMinor = (ticketPrice * 20) / 100;
+        document.getElementById('ticket_price').innerHTML = `${ticketMinor.toFixed(2)} &euro;`;
         console.log('Prezzo minorenni: ' + ticketMinor.toFixed(2));
     }
     // Biglietto Over 65
     else if (ageRange == 'Over 65') {
         let ticketOver65 = (ticketPrice * 40) / 100;
+        document.getElementById('ticket_price').innerHTML = `${ticketOver65.toFixed(2)} &euro;`;
         console.log('Prezzo Over 65: ' + ticketOver65.toFixed(2));
     }
     // Biglietto Standard/Maggiorenni 
     else {
+        document.getElementById('ticket_price').innerHTML = `${ticketPrice} &euro;`;
         console.log('Prezzo standard: ' + ticketPrice);
     }
+
+    // Mostro il biglietto
+
+    // Nome e cognome del passeggero
+    document.getElementById('user_name').innerHTML = nameLastname;
+
+    // Tipo di offerta/sconto
+    document.getElementById('ticket_type').innerHTML = ageRange;
+
+    // Numero della carrozza
+    document.getElementById('carriage').innerHTML = Math.floor(Math.random() * 10) + 1;;
+
+    // Codice CP
+    document.getElementById("cp_code").innerHTML = Math.floor(Math.random() * 10000) + 90001;
+
 }
